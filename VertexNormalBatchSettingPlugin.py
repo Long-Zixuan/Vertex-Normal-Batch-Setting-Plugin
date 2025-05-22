775a2ca2de027cd99e7888f00957a10eef477d52
Long-Zixuan <778275146@qq.com>
2025-05-22 15:47:04 +0800

    写了注释   LZX-git-2025-05-22-002

--- VertexNormalBatchSettingPlugin.py
+++ VertexNormalBatchSettingPlugin.py
@@ -6,19 +6,18 @@ from bpy.props import FloatVectorProperty
 from bpy.types import Operator, Panel
 from mathutils import Vector
 
-
 languages = {
     'en_US': {
-         "Vertex Normal Batch Setting Plugin": "Vertex Normal Batch Setting Plugin",
+        "Vertex Normal Batch Setting Plugin": "Vertex Normal Batch Setting Plugin",
         "Apply Normals": "Apply Normals",
-        "LoongLy:No mesh objects selected":"LoongLy:No mesh objects selected",
-        "LoongLy:Vertex normals set to":"LoongLy:Vertex normals set to"
+        "LoongLy:No mesh objects selected": "LoongLy:No mesh objects selected",
+        "LoongLy:Vertex normals set to": "LoongLy:Vertex normals set to"
     },
     'zh_CN': {
         "Vertex Normal Batch Setting Plugin": "顶点法相批量设置插件",
         "Apply Normals": "应用法线",
-        "LoongLy:No mesh objects selected":"LoongLy:没有被选中的物体",
-        "LoongLy:Vertex normals set to":"LoongLy：法线已经设置为"
+        "LoongLy:No mesh objects selected": "LoongLy:没有被选中的物体",
+        "LoongLy:Vertex normals set to": "LoongLy：法线已经设置为"
     }
 }
 
@@ -34,12 +33,12 @@ bl_info = {
     "category": "Object",
 }
 
+
 def detect_system_language():
     default_locale = locale.getdefaultlocale()
-    if(default_locale[0] in languages.keys()):
+    if (default_locale[0] in languages.keys()):
         return default_locale[0]
     return "en_US"
-
 
 
 class OBJECT_OT_SetVertexNormals(Operator):
@@ -51,10 +50,10 @@ class OBJECT_OT_SetVertexNormals(Operator):
         global lang_code
         # 获取目标法线向量
         target_normal = Vector(context.scene.target_normal).normalized()
-        
+
         # 获取所有选中的网格物体
         selected_meshes = [obj for obj in context.selected_objects if obj.type == 'MESH']
-        
+
         if not selected_meshes:
             self.report({'ERROR'}, languages[lang_code]["LoongLy:No mesh objects selected"])
             return {'CANCELLED'}
@@ -75,31 +74,33 @@ class OBJECT_OT_SetVertexNormals(Operator):
 
             # 更新网格以应用更改
             mesh.update()
-            #以下代码仅为了让第一次在编辑模式启动该脚本的时候显示正确的法线，第二次进入编辑模式将无法正确显示法相
-             # 确保处于编辑模式
+            # 以下代码段即使删除也不会对模型法线有影响，仅为了让在编辑模式时显示编辑后的法线，注意！第二次进入编辑模式将无法显示编辑后的法线
+            # 因为blander的normal update机制会把法线更新为blander自己计算的法线，但是我们将不会再在该模型使用blander的法线而是normals里面的数据
+            # 这个机制太坑了！我今天都搭载这上面了！！！！
+            # 确保处于编辑模式
             if obj_mode == 'EDIT':
                 bpy.ops.object.mode_set(mode='EDIT')
 
-                #mesh = bmesh.new()
-                #mesh.from_mesh(obj.data)
+                # mesh = bmesh.new()
+                # mesh.from_mesh(obj.data)
 
                 mesh = bmesh.from_edit_mesh(obj.data)
 
                 for v in mesh.verts:
                     v.normal = target_normal
 
-                #for f in mesh.faces:
+                # for f in mesh.faces:
                 #    f.normal = target_normal
 
                 bmesh.update_edit_mesh(obj.data)
-                #mesh.to_mesh(obj.data)
+                # mesh.to_mesh(obj.data)
                 mesh.free()
             bpy.context.view_layer.update()
             bpy.ops.object.mode_set(mode=obj_mode)
 
             # bpy.ops.object.mode_set(mode=obj_mode)
 
-        self.report({'INFO'}, languages[lang_code]["LoongLy:Vertex normals set to"]+f" {target_normal}")
+        self.report({'INFO'}, languages[lang_code]["LoongLy:Vertex normals set to"] + f" {target_normal}")
         return {'FINISHED'}
 
 
@@ -113,12 +114,12 @@ class VIEW3D_PT_NormalSetter(Panel):
         global lang_code
         layout = self.layout
         scene = context.scene
-        
+
         # 输入向量框
         layout.prop(scene, "target_normal", text="normal")
-        
+
         # 应用按钮
-        layout.operator("object.set_vertex_normals", text=languages[lang_code]["Apply Normals"] , icon='NORMALS_VERTEX')
+        layout.operator("object.set_vertex_normals", text=languages[lang_code]["Apply Normals"], icon='NORMALS_VERTEX')
 
 
 def register():
@@ -142,7 +143,6 @@ def unregister():
 
 
 if __name__ == "__main__":
-    register() 
+    register()
 
-
-#LZX-Pycharm2021.3-2025-05-22-001
+    # LZX-Pycharm2021.3-2025-05-22-001
